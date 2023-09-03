13948c925639363366630ee2dc3307ecc75269b9
Wilson <wilson.diniz@outlook.com.br>
2023-09-03 17:51:18 -0300

    Aula 5

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -31,7 +31,12 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento )
+  }
 
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento>{
+    pensamento.favorito = !pensamento.favorito
+    const url = `${this.API}/${pensamento.id}`
+    return this.editar(pensamento)
   }
 
   excluir(id: number): Observable<Pensamento> {
