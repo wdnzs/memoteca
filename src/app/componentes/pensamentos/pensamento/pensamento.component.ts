ab104b11f1d35fb3c7528baf21a2c5afbad09913
Wilson <wilson.diniz@outlook.com.br>
2023-09-03 17:10:39 -0300

    Aula 5 - Parcial

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -12,7 +12,8 @@ export class PensamentoComponent implements OnInit {
     id: 0,
     conteudo: 'I love Angular',
     autoria: 'Nay',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
 
   constructor() { }
@@ -25,6 +26,13 @@ export class PensamentoComponent implements OnInit {
       return 'pensamento-g'
     }
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if(this.pensamento.favorito == false){
+      return 'inativo'
+    }
+      return 'ativo'
   }
 
 }
